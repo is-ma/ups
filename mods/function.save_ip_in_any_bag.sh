cfd804cf34eca3da7ef32186b313120ac97c3e61
Is Ma <dotpointing@gmail.com>
2023-04-07 10:10:29 -0500

    Add dataforseo.com (nslookup) as a legal crawler

--- mods/function.save_ip_in_any_bag.sh
+++ mods/function.save_ip_in_any_bag.sh
@@ -23,6 +23,7 @@ save_ip_in_any_bag () {
   crawler_dns+=("yandex.ru.")
   crawler_dns+=("yandex.net.")
   crawler_dns+=("yandex.com.")
+  crawler_dns+=("dataforseo.com.")
 
   # let's find if the IP is a legal crawler
   dns_lookup=$(host $ip)
