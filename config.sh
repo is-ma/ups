ec6a53216b7c29b36a9bfa4d2eaac894525071c9
Is Ma <dotpointing@gmail.com>
2023-04-17 23:26:20 -0600

    Max IP request/min: 100 -> 60

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@
 # configuration
 export UFW_POLICE_PATH=/home/deploy/.ufw_police
 export WEBLOG_PATH=/var/log/nginx/access.log
-export MAX_IP_REQUESTS_PER_MIN_WITHOUT_BANNING=100
+export MAX_IP_REQUESTS_PER_MIN_WITHOUT_BANNING=60
 export PUBLIC_TXT_FOR_TASKER=/home/deploy/rank_app/code/public/lastminuteplus.txt
