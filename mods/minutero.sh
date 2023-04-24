f743d802b8311d9f93b298785c47156501e45ffb
Is Ma <dotpointing@gmail.com>
2023-04-23 23:14:45 -0600

    minutero.sh uses function.get_ip_plus.sh

--- mods/minutero.sh
+++ mods/minutero.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
+source $IS_MA__UPS_PATH/mods/function.get_ip_plus.sh
 
-# intro
 # - beautifyraw_minutero.log with minutero.log
 # - show the verified DNS sign in the log
 
 # example: 20230402 09:59 87 10 52.167.144.69
 last_log=$(tail -n1 $IS_MA__UPS_PATH/logs/raw_minutero.log)
 
-# get last IP, we will change it whith $ip_plus in the new log
+# ip -> ip_plus
 ip=$(echo $last_log | awk '{print $5}')
+ip_plus=$(get_ip_plus $ip)
 
-# now get the $ip_plus (example: 52.167.144.69 msn.com.) and
-# if not found an IP on whitelist.log, at least will be $ip
-ip_plus=$ip
-if grep -q "^$ip " $IS_MA__UPS_PATH/logs/whitelist.log; then
-  ip_plus=$(grep "^$ip " $IS_MA__UPS_PATH/logs/whitelist.log)
-fi
-
-# let's replace $ip with $ip_plus, delete last (.) and log!
 new_log=$(echo $last_log | sed "s/$ip/$ip_plus/")
-new_log=$(echo $new_log | sed 's/\.$//')
 echo $new_log | \
   awk '{
    if( $NF == 5 )
