57c998c3df07e1398d6a84ec04bdc57c19f1bc58
Is Ma <dotpointing@gmail.com>
2023-04-29 08:06:33 -0600

    Add alias 'b'

--- mods/aliases.sh
+++ mods/aliases.sh
@@ -3,6 +3,7 @@ source ~/.is-ma/ups/mods/function.get_ups_access_log.sh
 
 # show aliases
 echo "### UFW POLICE & STATS (UPS) ### [aliases & utilities]"
+echo "     b: Show banned IPs."
 echo "     r: Show real traffic."
 echo "     m: Tail last 60 rows on logs/minutero.log."
 echo "    sm: Search logs/minutero.log (usually IPs or time range)."
@@ -12,6 +13,7 @@ echo "   sah SEARCH [DATE] : Histogram URL view of an Nginx log search."
 echo "     t SEARCH [DATE] : Top NGINX log stats."
 
 # define aliases
+alias b="tail -n60 ~/.is-ma/ups/logs/banned_ips.log"
 alias r="sudo tailf /var/log/nginx/ups_access.log | awk -f ~/.is-ma/ups/mods/realtraffic.awk"
 alias m="tail -n60 ~/.is-ma/ups/logs/minutero.log"
 alias sm="cat ~/.is-ma/ups/logs/minutero.log | egrep -i"
