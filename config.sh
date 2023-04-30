836f88708e95bb664c0c1ba02ea632b123fd1fa6
Is Ma <dotpointing@gmail.com>
2023-04-29 18:28:56 -0600

    Filters: one filter per line

--- config.sh
+++ config.sh
@@ -7,8 +7,54 @@ export IS_MA__MAX_REQ_MIN=100
 export IS_MA__PUBLIC_TXT_FOR_TASKER=/home/deploy/rank_app/code/public/lastminuteplus.txt
 
 # offender xploit filters
-ending_in="\.asp |\.aspx |\.cgi |\.dll |\.env |\.jhtml |\.jsa |\.pl |/setup\.txt |/setup\.php |/eval\-stdin\.php "
-having_exactly=" /wp\-login\.php | /xmlrpc\.php "
-starting_with=" /\.git/| /\.aws/| /__phpmyadmin| /_phpmyadmin| /2phpmyadmin/| /actuator/| /administrator/| /admin/| /api/| /aws/| /core/| /dbadmin/| /db/| /debug/| /laravel/| /myadmin/| /mysql| /phpmy\-admin/| /phpmyadmin| /phppma/| /pma/| /pools/| /private/| /program/| /shopdb/| /sites/| /sql/| /sqlmanager/| /system/| /wordpress/| /wp/| /wp\-admin/| /wp\-content/| /wp\-includes/| /test/| /uploads/"
+ending_in="\.asp "\
+"|\.aspx "\
+"|\.cgi "\
+"|\.dll "\
+"|\.env "\
+"|\.jhtml "\
+"|\.jsa "\
+"|\.pl "\
+"|/setup\.txt "\
+"|/setup\.php "\
+"|/eval\-stdin\.php "
+having_exactly=" /wp\-login\.php "\
+"| /xmlrpc\.php "
+starting_with=" /\.git/"\
+"| /\.aws/"\
+"| /__phpmyadmin"\
+"| /_phpmyadmin"\
+"| /2phpmyadmin/"\
+"| /actuator/"\
+"| /administrator/"\
+"| /admin/"\
+"| /api/"\
+"| /aws/"\
+"| /core/"\
+"| /dbadmin/"\
+"| /db/"\
+"| /debug/"\
+"| /laravel/"\
+"| /myadmin/"\
+"| /mysql"\
+"| /phpmy\-admin/"\
+"| /phpmyadmin"\
+"| /phppma/"\
+"| /pma/"\
+"| /pools/"\
+"| /private/"\
+"| /program/"\
+"| /shopdb/"\
+"| /sites/"\
+"| /sql/"\
+"| /sqlmanager/"\
+"| /system/"\
+"| /wordpress/"\
+"| /wp/"\
+"| /wp\-admin/"\
+"| /wp\-content/"\
+"| /wp\-includes/"\
+"| /test/"\
+"| /uploads/"
 
 export IS_MA__XPLOIT_DIC="$ending_in|$having_exactly|$starting_with"
