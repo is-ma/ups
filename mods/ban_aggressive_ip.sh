91f12582ae535238953cc22c6a9619176ea4fe3a
Is Ma <dotpointing@gmail.com>
2023-05-08 17:15:45 -0600

    Re-code mods/ban_aggressive_ip.sh

--- mods/ban_aggressive_ip.sh
+++ mods/ban_aggressive_ip.sh
@@ -4,19 +4,19 @@
 # - this module will ban aggressive (many hits) black IPs
 
 # get last minute number of hits and the IP address
-hits=$(tail -n1 $IS_MA__UPS_PATH/logs/raw_minutero.log | cut -d' ' -f4)
-ip=$(tail -n1 $IS_MA__UPS_PATH/logs/raw_minutero.log | cut -d' ' -f5)
-now=$(tail -n1 $IS_MA__UPS_PATH/logs/raw_minutero.log | cut -c -14)
+hits=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $4}')
+ip=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $5}')
+domain=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $6}')
+now=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | cut -c -17)
 
-# if the IP is in blacklist.log
-if grep -q "^$ip$" $IS_MA__UPS_PATH/logs/blacklist.log; then
-
-  # ... if the IP is being aggressive
+# if domain is in the friendly_crawlers.conf, quit
+if grep -q " $domain$" $IS_MA__UPS_PATH/mods/ipplus/friendly_crawlers.conf; then 
+  : # friendly traffic: do nothing
+else
+  # mmm... is this IP being aggressive?
   if [ $hits -gt $IS_MA__MAX_REQ_MIN ]; then 
-
     # ... well, fire!
     sudo ufw insert 1 deny from $ip to any > /dev/null 2>&1
-    echo "$now:00 $hits $ip" >> $IS_MA__UPS_PATH/logs/banned_ips.log
-
+    echo "$now $hits $ip" >> $IS_MA__UPS_PATH/logs/banned_ips.log
   fi
 fi
