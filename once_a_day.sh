362836fd09399a4fe94160a1c5605ab3560387bf
Is Ma <dotpointing@gmail.com>
2023-05-09 11:21:10 -0600

    Bug: once_a_day.sh now runs before 00:00h, fix the log style

--- once_a_day.sh
+++ once_a_day.sh
@@ -3,11 +3,14 @@
 # read configuration file
 source /home/deploy/.is-ma/ups/config.sh
 
-# variables
+# date variables
+today=$(date '+%d/%b/%Y')  # executes from logrotate before 00:00
+#yesterday=$(date --date='1 day ago' '+%d/%b/%Y')  # executes from logrotate at 00:00
+abbrev_day=$(date --date='1 day ago' '+%a')
+
+# other variables
 total_hits=$(sudo cat $IS_MA__WEBLOG_PATH | wc -l)
 uniq_ip_count=$(sudo cat $IS_MA__WEBLOG_PATH | awk '{print $3}' | sort -nu | wc -l)
-yesterday=$(date --date='1 day ago' '+%d/%b/%Y')  # executes from logrotate at 00:00
-abbrev_day=$(date --date='1 day ago' '+%a')
 
 # add a new record to $IS_MA__UPS_PATH/logs/daily.log
 printf "%8d %6d %s %s\n" $total_hits $uniq_ip_count $yesterday $abbrev_day >> $IS_MA__UPS_PATH/logs/daily.log
