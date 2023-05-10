7aacb19b2bcde910c3a32719ea7b2bcc4ff52112
Is Ma <dotpointing@gmail.com>
2023-05-10 14:58:56 -0600

    Robust ban scripts (catch multiple simultaneous aggressors

--- mods/ban_aggressive_ip.sh
+++ mods/ban_aggressive_ip.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 source ~/.is-ma/ups/config.sh
+source $IS_MA__UPS_PATH/mods/ipplus/function.iptoplus.sh
 
-# intro
-# - this module will ban aggressive (many hits) black IPs
+# Find last minute aggressive unfriendly crawlers
 
-# get last minute number of hits and the IP address
-hits=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $4}')
-ip=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $5}')
-domain=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | awk '{print $6}')
-now=$(tail -n1 $IS_MA__UPS_PATH/logs/minutero.log | cut -c -17)
+# This script will find last minute aggressive unfriendly crawlers,
+# will ban those IPs, will report those IPs (logs/banned_ips.log),
+# and use report style: 20230426 15:33:50 207.46.13.218 199
 
-# if domain is in the friendly_crawlers.conf, quit
-if grep -q " $domain$" $IS_MA__UPS_PATH/mods/ipplus/friendly_crawlers.conf; then 
-  : # friendly traffic: do nothing
-else
+one_min_ago=$(date --date='1 minute ago' '+%Y%m%d %H:%M')
+# if IP behaves aggressively a minute ago
+sudo tail -n2000 $IS_MA__WEBLOG_PATH | grep "$one_min_ago" | awk '{print $3}' | sort | uniq -c | sort -nr | while read -r line; do
+
+  # split $line '4 52.6.243.18'
+  hits=$(echo $line | awk '{print $1}')
+  ip=$(echo $line | awk '{print $2}')
+
   # mmm... is this IP being aggressive?
-  if [ $hits -gt $IS_MA__MAX_REQ_MIN ]; then 
-    # ... well, fire!
-    sudo ufw insert 1 deny from $ip to any > /dev/null 2>&1
-    echo "$now $hits $ip" >> $IS_MA__UPS_PATH/logs/banned_ips.log
+  if [ $hits -gt $IS_MA__MAX_REQ_MIN ]; then
+
+    # and if IP is already blocked
+    if egrep -q " $ip " $IS_MA__UPS_PATH/logs/banned_ips.log; then
+      :  # command that does nothing
+      #echo "DEBUG: IP was found on /logs/banned_ips.log" >> $IS_MA__UPS_PATH/logs/banned_ips.log
+    else
+
+      # iptoplus($ip): 167.172.74.244 ---
+      dnsdomain=$( iptoplus $ip | awk '{print $2}' )  # ---
+      if grep -q " $dnsdomain$" $IS_MA__UPS_PATH/mods/ipplus/friendly_crawlers.conf; then
+        # friendly: only log, never ban
+        echo "$one_min_ago:00 $ip $hits #WhitelistedNotBanned" >> $IS_MA__UPS_PATH/logs/banned_ips.log
+      else
+        # unfriendly: log & ban -> 20230509 11:39:05 /wp-login.php 87.116.133.201
+        echo "$one_min_ago:00 $ip $hits" >> $IS_MA__UPS_PATH/logs/banned_ips.log
+        sudo ufw insert 1 deny from $ip to any > /dev/null 2>&1
+      fi
+
+    fi
   fi
-fi
+done
--- mods/ban_xploiter_ips.sh
+++ mods/ban_xploiter_ips.sh
@@ -10,20 +10,20 @@ source $IS_MA__UPS_PATH/mods/ipplus/function.iptoplus.sh
 
 one_min_ago=$(date --date='1 minute ago' '+%Y%m%d %H:%M')
 # if IP behaves aggressively a minute ago
-# log style: 20230426 15:33:50 /41080-club-herbalife 207.46.13.218
-sudo tail -n2000 $IS_MA__WEBLOG_PATH | grep "$one_min_ago" | awk '{print $4,$5,$2,$3}' | egrep -i "$IS_MA__XPLOIT_DIC" | while read -r line; do
+sudo tail -n2000 $IS_MA__WEBLOG_PATH | grep "$one_min_ago" | awk '{print $4,$5,$3,$2}' | egrep -i "$IS_MA__XPLOIT_DIC" | while read -r line; do
 
-  ip=$(echo $line | awk '{print $4}')
+  # get ip from $line '20230509 16:04:59 66.249.66.38 /1661200-antonela'
+  ip=$(echo $line | awk '{print $3}')
 
   # and if IP is already blocked
-  if egrep -q " $ip$" $IS_MA__UPS_PATH/logs/banned_ips.log; then
+  if egrep -q " $ip " $IS_MA__UPS_PATH/logs/banned_ips.log; then
     :  # command that does nothing
     #echo "DEBUG: IP was found on /logs/banned_ips.log" >> $IS_MA__UPS_PATH/logs/banned_ips.log
   else
 
-    # iptoplus $ip: 167.172.74.244 --- # domain: ---
-    domain=$( iptoplus $ip | awk '{print $2}' )
-    if grep -q " $domain$" $IS_MA__UPS_PATH/mods/ipplus/friendly_crawlers.conf; then
+    # iptoplus($ip): 167.172.74.244 ---
+    dnsdomain=$( iptoplus $ip | awk '{print $2}' )  # ---
+    if grep -q " $dnsdomain$" $IS_MA__UPS_PATH/mods/ipplus/friendly_crawlers.conf; then
       # friendly: only log, never ban
       echo "$line #WhitelistedNotBanned" >> $IS_MA__UPS_PATH/logs/banned_ips.log
     else
